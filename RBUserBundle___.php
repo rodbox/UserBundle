70004e939f3bad55b5aebf14c06a0c8a2b3c1984
rodbox <rodolphe@rodbox.fr>
2016-04-03 22:30:16 +0200

    upd

---
+++ RBUserBundle___.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RB\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RBUserBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
+?>
