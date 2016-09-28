7f229d63456158d5542b863c5c1c52370a053e7b
Rodolphe <jacquemin.rodolphe@hotmail.fr>
2016-09-28 14:27:39 +0200

    fix name

--- Twig/RBUserExtension.php
+++ Twig/RBUserExtension.php
@@ -11,17 +11,23 @@ class RBUserExtension  extends \Twig_Extension{
         $this->session   = $session;
     }
 
+
+
     public function userImg($id= '1', $format = 'C')
     {
         $img  = 'log.jpg';
         echo $this->twig->render('RBUserBundle:Img:img-'.$format.'.html.twig',['id'=>$id, 'img'=>$img]);
     }
 
+
+
     public function myImg($format = 'C')
     {
         $id = '1';
         $this->userImg($id, $format);
     }
+
+
 
     public function getName(){
         return 'rb_user_extension';
@@ -31,8 +37,8 @@ class RBUserExtension  extends \Twig_Extension{
 
     public function getFunctions(){
         return array(
-            new \Twig_SimpleFunction("rb_user_img", [$this, 'userImg'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction("rb_my_img", [$this, 'myImg'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction("userImg", [$this, 'userImg'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction("myImg", [$this, 'myImg'], ['is_safe' => ['html']]),
         );
     }
 }
